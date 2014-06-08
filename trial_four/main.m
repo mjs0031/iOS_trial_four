bff4a04e5dc540e2fd2873c9e213267ba131616d
Matthew J Swann <mjs0031@auburn.edu>
2014-06-08 09:35:41 -0500

    Initial Commit

---
+++ trial_four/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  trial_four
+//
+//  Created by Matthew Swann on 6/8/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "trial_fourAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([trial_fourAppDelegate class]));
+    }
+}
---
+++ trial_four/trial_fourAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  trial_fourAppDelegate.h
+//  trial_four
+//
+//  Created by Matthew Swann on 6/8/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface trial_fourAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
